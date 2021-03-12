8fe48dab4c1889611729556703a67b9f5b768c61
Sami-Betka <sami1206@hotmail.fr>
2021-03-12 02:19:24 +0100

    .

--- src/main/java/combibet/controller/MainController.java
+++ src/main/java/combibet/controller/MainController.java
@@ -1,10 +1,12 @@
 package combibet.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
+	@GetMapping
 	public String home() {
 		
 		return "home";
