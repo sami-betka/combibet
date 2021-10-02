c1be5875c3f9ae20cb60d71839990bec3b0bb8d0
sami-betka <sami1206@hotmail.fr>
2021-10-02 12:07:36 +0200

    k

--- src/main/java/combibet/controller/TwilioController.java
+++ src/main/java/combibet/controller/TwilioController.java
@@ -11,24 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 import combibet.service.TwilioService;
 import combibet.twilio.SmsRequest;
 
-//@RestController
-//@RequestMapping("api/v1/sms")
-//public class TwilioController {
-//
-//    private final TwilioService service;
-//
-//    @Autowired
-//    public TwilioController(TwilioService service) {
-//        this.service = service;
-//    }
-//
-////    @PostMapping
-////    public void sendSms(@Valid @RequestBody SmsRequest smsRequest) {
-////        service.sendSms(smsRequest);
-////    }
-//    
+@RestController
+@RequestMapping("api/v1/sms")
+public class TwilioController {
+
+    private final TwilioService service;
+
+    @Autowired
+    public TwilioController(TwilioService service) {
+        this.service = service;
+    }
+
 //    @PostMapping
-//    public void sendSms(SmsRequest smsRequest) {
+//    public void sendSms(@Valid @RequestBody SmsRequest smsRequest) {
 //        service.sendSms(smsRequest);
 //    }
-//}
+    
+    @PostMapping
+    public void sendSms(SmsRequest smsRequest) {
+        service.sendSms(smsRequest);
+    }
+}
