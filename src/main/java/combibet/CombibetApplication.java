51302250a1c0b886959b16ac8d9d04d4eb19f077
sami-betka <sami1206@hotmail.fr>
2022-01-05 23:04:08 +0100

    first commit

--- src/main/java/combibet/CombibetApplication.java
+++ src/main/java/combibet/CombibetApplication.java
@@ -6,13 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import combibet.entity.Bankroll;
 import combibet.entity.Bet;
-import combibet.entity.BetStatus;
-import combibet.entity.ConfidenceIndex;
-import combibet.entity.Discipline;
 import combibet.entity.HorseRacingBet;
+import combibet.repository.BankrollRepository;
 import combibet.repository.BetRepository;
-import combibet.repository.HorseRacingBetRepository;
 
 @SpringBootApplication
 public class CombibetApplication {
@@ -21,8 +19,78 @@ public class CombibetApplication {
 		ApplicationContext ctx = SpringApplication.run(CombibetApplication.class, args);
 		
 //		BankrollRepository bankrollRepository = ctx.getBean(BankrollRepository.class);
-//		Bankroll bank = bankrollRepository.findById(2l).get();
+//		BetRepository betRepository = ctx.getBean(BetRepository.class);
 //
+//		Bankroll bank = bankrollRepository.findById(59l).get();
+//		List<Bet> betList = bank.getBets();
+//		Bankroll bank2 = bankrollRepository.findById(54l).get();
+//		List<Bet> betList2 = bank2.getBets();
+//		Bankroll bank3 = bankrollRepository.findById(55l).get();
+//		List<Bet> betList3 = bank3.getBets();
+//
+//		for (Bet b : betList2) {
+//			HorseRacingBet bet = (HorseRacingBet) b;
+//
+//			HorseRacingBet newBet = new HorseRacingBet();
+//			newBet.setAnte(bet.getAnte());
+//			newBet.setBankroll(bank);
+//			newBet.setConfidenceIndex(bet.getConfidenceIndex());
+//			newBet.setDate(bet.getDate());
+//			newBet.setGambler(bet.getGambler());
+//			newBet.setOdd(bet.getOdd());
+//			newBet.setSelection(bet.getSelection());
+//			newBet.setStatus(bet.getStatus());
+//			newBet.setType(bet.getType());
+//			newBet.setField(bet.getField());
+//			newBet.setDiscipline(bet.getDiscipline());
+//			newBet.setFormattedDate(bet.getFormattedDate());
+//			newBet.setId(null);
+//			
+//			Bet savedBet = betRepository.save(newBet);
+//			
+//			betList.add(savedBet);
+//		}
+//		for (Bet b : betList3) {
+//			HorseRacingBet bet = (HorseRacingBet) b;
+//
+//			HorseRacingBet newBet = new HorseRacingBet();
+//			newBet.setAnte(bet.getAnte());
+//			newBet.setBankroll(bank);
+//			newBet.setConfidenceIndex(bet.getConfidenceIndex());
+//			newBet.setDate(bet.getDate());
+//			newBet.setGambler(bet.getGambler());
+//			newBet.setOdd(bet.getOdd());
+//			newBet.setSelection(bet.getSelection());
+//			newBet.setStatus(bet.getStatus());
+//			newBet.setType(bet.getType());
+//			newBet.setField(bet.getField());
+//			newBet.setDiscipline(bet.getDiscipline());
+//			newBet.setFormattedDate(bet.getFormattedDate());
+//			newBet.setId(null);
+//			
+//			Bet savedBet = betRepository.save(newBet);
+//			
+//			betList.add(savedBet);
+//		}
+//		
+//		
+//		
+//		bank.setBets(betList);
+//		bankrollRepository.save(bank);
+//		
+//		System.out.println("Stop");
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//
 //		
 //		for(Bet bet: ) {
 //			bet.setConfidenceIndex(ConfidenceIndex.FIVE);
